5d20222270df0b94bfacdf884461350ce5a5ebc8
KROOKIMWOOBIN <woodol4540@naver.com>
2023-06-06 19:07:09 +0900

    Day0606

--- vgp/src/App.vue
+++ vgp/src/App.vue
@@ -2,28 +2,38 @@
   <div>
     <header>
       <nav class="sticky-nav">
-      <router-link to="/">
-        <h1 class="game_title">FPS</h1>
-      </router-link>
-      <router-link to="/" exact>홈</router-link>
-      <span class="separator">|</span>
-      <router-link to="/about">커뮤니티</router-link>
-    </nav>
+        <router-link to="/">
+          <h1 :style="{ color: titleColor, textShadow: titleShadow }" class="game_title">FPS</h1>
+        </router-link>
+        <router-link to="/" exact>홈</router-link>
+        <span class="separator">|</span>
+        <router-link to="/about">커뮤니티</router-link>
+      </nav>
     </header>
     <router-view />
     <footer class="footer">
       <p>Created by Kim Woo-bin<br>Email: woodol4540@naver.com</p>
       <button class="upbtn" @click="scrollToTop">UP</button>
     </footer>
-  </div><br><br><br><br><br>
+  </div>
 </template>
 
 <style>
 @import "@/assets/App.css";
+
+.game_title {
+  text-shadow: 2px 2px 4px silver; /* 그림자 초기값 */
+}
 </style>
 
 <script>
 export default {
+  data() {
+    return {
+      titleColor: "silver",
+      titleShadow: "2px 2px 4px silver",
+    };
+  },
   methods: {
     scrollToTop() {
       let intervalId = setInterval(() => {
@@ -35,21 +45,33 @@ export default {
         }
       }, 10);
     },
+    getRandomColor() {
+      const letters = "0123456789ABCDEF";
+      let color = "#";
+      for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    },
   },
   mounted() {
-    window.addEventListener('wheel', this.handleScroll);
+    window.addEventListener("wheel", this.handleScroll);
+    setInterval(() => {
+      const color = this.getRandomColor();
+      this.titleColor = color;
+      this.titleShadow = `2px 2px 4px ${color}`;
+    }, 100); // 1초마다 변경 (원하는 간격으로 수정 가능)
   },
   unmounted() {
-    window.removeEventListener('wheel', this.handleScroll);
+    window.removeEventListener("wheel", this.handleScroll);
   },
   handleScroll(event) {
     event.preventDefault();
     const delta = Math.sign(event.deltaY);
     window.scrollBy({
       top: window.innerHeight * delta,
-      behavior: 'smooth',
+      behavior: "smooth",
     });
   },
 };
 </script>
-
